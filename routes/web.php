be55bf1f5a2eb4b8e2b67e464cd815e6443d02ff
miraz66 <mirajulislam0509@gmail.com>
2024-03-04 17:18:16 +0600

    Laravel test

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return response("<h1>Hello world</h1>");
+});
+
+Route::get('/posts/{id}', function ($id) {
+    dd($id);
+    return response("<h1>Posts</h1>" . $id . "</h1>", status: 200);
+})-> where('id', '[0-9]+');
