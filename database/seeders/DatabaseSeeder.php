af96216b05111e42d631bd1608a2a0ac7ce42eb8
miraz66 <mirajulislam0509@gmail.com>
2024-03-13 16:43:56 +0600

    Register

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
@@ -13,9 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
+        
+        $user = User::factory()->create([
+            'name' => 'John Smith',
+            'email' => 'john@gmail.com',
+        ]);
 
-        Listing::factory(10)->create();
+        Listing::factory(10)->create([
+            'user_id' => $user->id
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
